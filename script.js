aec8377b3c068a04d9c8c7ea9671cc87dd918a62
polinanalivaichenko <polinaN08@yandex.ru>
2023-07-18 14:30:32 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".mySwiper", {
+    centeredSlides: true,
+    autoplay: {
+      delay:5500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+});
